fe199f5b9b4c1aa8259dc4e2d3157ab608a29f35
trm <tomasrabazo@gmail.com>
2018-05-02 19:28:01 +0100

    Unicode examples

--- CharAndBoolean/src/com/timbuchalka/Main.java
+++ CharAndBoolean/src/com/timbuchalka/Main.java
@@ -4,5 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        char myChar = '\u00A9';
+        System.out.println("Unicode:" + myChar);
+
+        boolean myBoolean = true;
+        boolean isMale = true;
+
+        // U+00AE
+        char myRegisteredSign = '\u00AE';
+        System.out.println("Unicode:" + myRegisteredSign);
     }
 }
